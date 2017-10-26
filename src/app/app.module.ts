7df61c85a88b0912a072979bf1f2536e55dc5b31
Shahzad Hussain <shahzad@canbrand.in>
2017-10-26 18:30:17 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    DepartmentListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {'path':"employee","component":EmployeeListComponent},
+      {'path':"department","component":DepartmentListComponent}
+  ])
   ],
   providers: [],
   bootstrap: [AppComponent]
