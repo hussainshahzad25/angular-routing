eeb20ea87277a67f13d8ca7bd59837cf68459280
shahzad <shahzad@canbrand.in>
2017-11-04 20:58:55 +0530

    latest changes made on 4 oct home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { HttpModule } from '@angular/http';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import {RouterModule} from '@angular/router';
 
-import { RouterModule } from '@angular/router';
-
-
-
-import { SimpleNotificationsModule } from 'angular2-notifications';
+import {SimpleNotificationsModule} from 'angular2-notifications';
 import 'angular2-notifications';
-import { ToastyModule } from 'ng2-toasty';
+import {ToastyModule} from 'ng2-toasty';
 
+import {AppComponent} from './app.component';
+import {EmployeeListComponent} from './employee-list/employee-list.component';
+import {DepartmentListComponent} from './department-list/department-list.component';
+import {LoginComponent} from './login/login.component';
+import {RegisterComponent} from './register/register.component';
+import {NavComponent} from './nav/nav.component';
+import {HomeComponent} from './home/home.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
 
-import { AppComponent } from './app.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
-import { DepartmentListComponent } from './department-list/department-list.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { NavComponent } from './nav/nav.component';
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-
-import { LoginService } from './login/login.service';
-import { RegisterService } from './register/register.service';
-import { NavService } from './nav/nav.service';
-import { DepartmentdetailsComponent } from './departmentdetails/departmentdetails.component';
-import { UploadComponent } from './upload/upload.component';
-
-
+import {LoginService} from './login/login.service';
+import {RegisterService} from './register/register.service';
+import {NavService} from './nav/nav.service';
+import {DepartmentdetailsComponent} from './departmentdetails/departmentdetails.component';
+import {UploadComponent} from './upload/upload.component';
+import {ForgetpasswordComponent} from './forgetpassword/forgetpassword.component';
+import {NewloginComponent} from './newlogin/newlogin.component';
+import { HelpComponent } from './help/help.component';
+import { Login1Component } from './login1/login1.component';
+import { Login2Component } from './login2/login2.component';
+import { Login3Component } from './login3/login3.component';
+import { Login4Component } from './login4/login4.component';
+import { Login5Component } from './login5/login5.component';
 
 @NgModule({
   declarations: [
@@ -43,35 +45,97 @@ import { UploadComponent } from './upload/upload.component';
     HomeComponent,
     DashboardComponent,
     DepartmentdetailsComponent,
-    UploadComponent
-
+    UploadComponent,
+    ForgetpasswordComponent,
+    NewloginComponent,
+    HelpComponent,
+    Login1Component,
+    Login2Component,
+    Login3Component,
+    Login4Component,
+    Login5Component
 
   ],
   imports: [
     // PushNotificationsModule,
     BrowserModule,
-    FormsModule, ReactiveFormsModule, HttpModule, HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    HttpClientModule,
     RouterModule.forRoot([
-      { 'path': "", "component": LoginComponent },
-      { 'path': "home", "component": HomeComponent },
-      { 'path': "dashboard", "component": DashboardComponent },
+      {
+        'path': "",
+        "component": LoginComponent
+      }, {
+        'path': "home",
+        "component": HomeComponent
+      }, {
+        'path': "dashboard",
+        "component": DashboardComponent
+      }, {
+        'path': "employee",
+        "component": EmployeeListComponent,
+        data: {
+          title: 'Employee List'
+        }
+      }, {
+        'path': "department",
+        "component": DepartmentListComponent,
+        data: {
+          title: 'Department List'
+        }
+      }, {
+        'path': "department/:deptId",
+        "component": DepartmentdetailsComponent
+      }, {
+        'path': "login",
+        "component": LoginComponent
+      }, {
+        'path': "register",
+        "component": RegisterComponent
+      }, {
+        'path': "nav",
+        "component": NavComponent
+      }, {
+        'path': "forget",
+        "component": ForgetpasswordComponent
+      }, {
+        'path': "new-login",
+        "component": NewloginComponent
+      }
+      , {
+        'path': "help",
+        "component": HelpComponent
+      }
+      , {
+        'path': "login1",
+        "component": Login1Component
+      }
+      , {
+        'path': "login2",
+        "component": Login2Component
+      }
+      , {
+        'path': "login3",
+        "component": Login3Component
+      }
+      , {
+        'path': "login4",
+        "component": Login4Component
+      }, {
+        'path': "login5",
+        "component": Login5Component
+      }
 
-      { 'path': "employee", "component": EmployeeListComponent, data: { title: 'Employee List' } },
-
-      { 'path': "department", "component": DepartmentListComponent, data: { title: 'Department List' } },
-      { 'path': "department/:deptId", "component": DepartmentdetailsComponent },
-
-      { 'path': "login", "component": LoginComponent },
-      { 'path': "register", "component": RegisterComponent },
-      { 'path': "nav", "component": NavComponent },
-
-
-
+      
     ]),
     SimpleNotificationsModule.forRoot(),
     ToastyModule.forRoot()
   ],
-  providers: [LoginService, RegisterService, NavService],
+  providers: [
+    LoginService, RegisterService, NavService
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/newlogin/newlogin.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newlogin',
+  templateUrl: './newlogin.component.html',
+  styleUrls: ['./newlogin.component.css']
+})
+export class NewloginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  login(value){
+
+    console.log("Hii");
+  }
+}
